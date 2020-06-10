2b28b1474600caa514a01a5097326d557a03194e
Akshath <akshathmahajan13@gmail.com>
2020-06-10 23:59:56 +0530

    Added Better Dijkstra's Implementation
    Dijkstra_3(s,v) not only provides us with the speed optimizations of
    loading the data to RAM before performing operations on it, but also
    reduces the runtime from cubic to quadratic
    
    Benchmarks:
    
    Worst performance on map for specific source = s, destination = v:
    Dijkstra(s,v) = Unknown time
    Dijkstra_2(s,v) = 18Mins
    Dijkstra_3(s,v) = 27secs

--- TrailBlazer/Maps/algorithms.py
+++ TrailBlazer/Maps/algorithms.py
@@ -47,6 +47,7 @@ def Dijkstra(source, destination):
     return res
 
 def Dijkstra_2(source, destination):
+    print("DIJKSTRA_2")
     nearest_s_ID = DestinationNodes.objects.filter(name=source)[0].nearest_neighbour
     nearest_d_ID = DestinationNodes.objects.filter(name=destination)[0].nearest_neighbour
     starttime = time.time()
@@ -79,6 +80,52 @@ def Dijkstra_2(source, destination):
                         v_star = v
         A[w_star] = mincost
         X.append(w_star)
+        path[w_star] = v_star
+    print(time.time()-starttime)
+    t = nearest_d_ID
+    res = []
+    while(t != nearest_s_ID):
+        res.append(t)
+        t = path[t]
+    print(time.time()-starttime)
+    print(res)
+    return res
+
+def Dijkstra_3(source, destination):
+    nearest_s_ID = DestinationNodes.objects.filter(name=source)[0].nearest_neighbour
+    nearest_d_ID = DestinationNodes.objects.filter(name=destination)[0].nearest_neighbour
+    starttime = time.time()
+    #Load Graph into ram:
+    all_road_nodes = Nodes.objects.filter(isRoadNode=True)
+    all_edges = Edges.objects.all()
+    edges_in = {}
+    for v in all_road_nodes:
+        edges_in[v.name] = Edges.objects.filter(Q(node1=v.name)|Q(node2=v.name))
+
+    X = {roadnode.name:False for roadnode in all_road_nodes}  #Processed Node IDS
+    X[nearest_s_ID] = True
+    A = {nearest_s_ID:0}
+    path = {}
+    X_SET = [nearest_s_ID]
+    print(time.time()-starttime)
+    while X_SET[-1] != nearest_d_ID:
+        mincost = float('inf')
+        for v in X_SET: #v is ID
+            for edge in edges_in[v]:
+                if X[edge.node1] and X[edge.node2]:
+                    continue
+                dijkstra_greedy = A[v] + edge.cost
+                if dijkstra_greedy < mincost:
+                    mincost = dijkstra_greedy
+                    if edge.node1 == v:
+                        w_star = edge.node2
+                        v_star = v
+                    else:
+                        w_star = edge.node1
+                        v_star = v
+        A[w_star] = mincost
+        X[w_star] = True
+        X_SET.append(w_star)
         path[w_star] = v_star
     print(time.time()-starttime)
     t = nearest_d_ID
--- TrailBlazer/Maps/views.py
+++ TrailBlazer/Maps/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 import folium
 from .models import DestinationNodes
 from .models import Nodes
-from .algorithms import Dijkstra, Dijkstra_2
+from .algorithms import Dijkstra, Dijkstra_2, Dijkstra_3
 
 def home(req):
     return render(req, 'Maps/maps_home.html')
@@ -12,19 +12,17 @@ def map(request):
     #68.26% Height fits perfectly in the container and prevents a lot of css-overflow issues which are currently managed by overflow:auto
     #REPLACE LOCATION AND FILL DATABASE
     if request.method == 'POST':
-        map1 = folium.Map(location=[19.2120, 72.8567], zoom_start=14)
-        path = Dijkstra(7553183710,7082370249)
+        map1 = folium.Map(location=[19.2120, 72.8567], zoom_start=14)#LOCATION = s.coords+d.coords/2
+        s = request.POST['source']
+        d = request.POST['destination']
+        path = Dijkstra_3(s,d)
         for i in path:
-            print(i)
             ob = Nodes.objects.filter(name=i)[0]
             coord = [ob.latitude, ob.longitude]
-            folium.Marker(location=coord).add_to(map1)
+            folium.Marker(location=coord, popup=i).add_to(map1)
         m = map1._repr_html_()
-        #Add path to map before returning
-        #opt1 - remove all nodes except start and end and display map [will need to add reset button too as people cant remember all node ids]
-        #opt2 - Keep all destination nodes and just add path [no need to add reset map button]
-        #set location of  returning map to be average of node1coords and node2coords
         return render(request, 'Maps/maps_map.html', {'map':m})
+
     if request.method == 'GET':
         map1 = folium.Map(location=[19.2120, 72.8567], zoom_start=14)
         destnodes = DestinationNodes.objects.all()  #Destination nodes qs
@@ -33,9 +31,10 @@ def map(request):
             coord = (n.latitude, n.longitude)
             folium.Marker(location=coord, popup="NODE ID: "+str(n.name)).add_to(map1)
         m = map1._repr_html_()
-        #Add destination nodes to map before returning
         return render(request, 'Maps/maps_map.html', {'map':m})
-r1= Dijkstra(7553183710,7082370249)
-print("H")
-r2 = Dijkstra_2(7553183710,7082370249)
-print(r1 == r2)
+# s,v = 7553183710,7553183709
+# Dijkstra_2(s,v)
+# print("HERE")
+# Dijkstra_3(s,v)
+
+#7048041081,1939199158
